a69b8628c12bcd366f9323812bf0946db6768bdf
Weerasit <96558513+Weerasits@users.noreply.github.com>
2025-04-15 00:00:00 +0700

    Update connection_matlab.m

--- connection_matlab.m
+++ connection_matlab.m
@@ -2,12 +2,12 @@
 clc;
 t = tcpclient('127.0.0.1', 9999);
 
-disp("เชื่อมต่อกับ Python เรียบร้อย");
+disp("Connected");
 
 while true
     if t.NumBytesAvailable > 0
         data = read(t, t.NumBytesAvailable, "string");
-        disp("ข้อมูลที่ได้รับจาก Python: " + data);
+        disp("Data from Python: " + data);
     end
     pause(0.1); 
 end
